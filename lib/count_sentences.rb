ac0067740d0bb0db27bc3aab23a7f1f4086614c5
Nicholas Ng <nicholasykng@gmail.com>
2019-10-26 03:25:33 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,19 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    separate = self.split(/[.?!]/).reject {|x| x.empty?}
+    separate.count
   end
 end
